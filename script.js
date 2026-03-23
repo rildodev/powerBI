85cf8fc741d5d8aff24b1e2d05714e7d22ba2b53
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2026-03-23 14:33:50 -0300

    correções na API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // COLE AQUI O LINK DO SEU IFRAME DO POWER BI
-const powerBIUrl = "Chttps://app.powerbi.com/reportEmbed?reportId=97fb9fcc-1ffc-4f03-bece-22ed5fd0695d&autoAuth=true&ctid=312c3d0e-5d88-41b0-a404-437115eb5a9c";
+const powerBIUrl = "https://app.powerbi.com/reportEmbed?reportId=97fb9fcc-1ffc-4f03-bece-22ed5fd0695d&autoAuth=true&ctid=312c3d0e-5d88-41b0-a404-437115eb5a9c";
 
 // Carrega o iframe automaticamente
 document.addEventListener("DOMContentLoaded", () => {
