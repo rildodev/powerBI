c92f58c7aa810ffe2cb31b3d699eb694332bfc92
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2026-03-23 15:11:58 -0300

    Alterando o layout e view

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<iframe 
-    title="lab01"
-    src="https://app.powerbi.com/reportEmbed?reportId=97fb9fcc-1ffc-4f03-bece-22ed5fd0695d&autoAuth=true&ctid=312c3d0e-5d88-41b0-a404-437115eb5a9c"
-    allowFullScreen="true">
-</iframe>
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Painel carregado com sucesso!");
+});
