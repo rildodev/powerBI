65e4c376c585665002ce554f9cbb046cddb3e305
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2026-03-23 14:43:23 -0300

    + alteração

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Mensagem simples ao carregar
-window.addEventListener("load", () => {
-    console.log("Dashboard carregado com sucesso!");
+// Simulação de navegação ativa
+const itens = document.querySelectorAll(".sidebar li");
+
+itens.forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector(".active").classList.remove("active");
+        item.classList.add("active");
+    });
 });
