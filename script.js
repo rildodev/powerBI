5cb13fa40060de16792394a61aa456b5334d44d5
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2026-03-23 14:46:34 -0300

    + mudanças

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Simulação de navegação ativa
-const itens = document.querySelectorAll(".sidebar li");
-
-itens.forEach(item => {
-    item.addEventListener("click", () => {
-        document.querySelector(".active").classList.remove("active");
-        item.classList.add("active");
-    });
-});
+<iframe 
+    title="lab01"
+    src="https://app.powerbi.com/reportEmbed?reportId=97fb9fcc-1ffc-4f03-bece-22ed5fd0695d&autoAuth=true&ctid=312c3d0e-5d88-41b0-a404-437115eb5a9c"
+    allowFullScreen="true">
+</iframe>
